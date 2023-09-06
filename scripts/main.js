569582711d7664948e7028405d62a951993af275
augustomeneses <augusto.meneses@legalidaddigital.com.mx>
2023-09-05 22:28:50 -0600

    carga de sitio

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+let miImage = document.querySelector("img");
+miImage.onclick = function () {
+  let miSrc = miImage.getAttribute("src");
+  if (miSrc === "images/Legalidad-Digital-Logo-800x800.png") {
+    miImage.setAttribute("src", "images/second-image.jpg");
+  } else {
+    miImage.setAttribute("src", "images/Legalidad-Digital-Logo-800x800.png");
+  }
+};
+let miBoton = document.querySelector("button");
+let miTitulo = document.querySelector("h1");
+function estableceNombreUsuario() {
+  let miNombre = prompt("Por favor, ingresa tu nombre.");
+  if (!miNombre) {
+    estableceNombreUsuario();
+  } else {
+    localStorage.setItem("nombre", miNombre);
+    miTitulo.textContent = "Mozilla es genial, " + miNombre;
+  }
+}
+if (!localStorage.getItem("nombre")) {
+  estableceNombreUsuario();
+} else {
+  let nombreAlmacenado = localStorage.getItem("nombre");
+  miTitulo.textContent = "Mozilla es genial, " + nombreAlmacenado;
+}
+miBoton.onclick = function () {
+  estableceNombreUsuario();
+};
